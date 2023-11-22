2217322efc79f8bf7d8638cc409666304f51bc0b
Alan Henrique <alanhipermassa@gmail.com>
2023-11-22 08:27:34 -0300

    Criada a classe ItemVenda Classe usada para representar os itens dentro de um pedido

---
+++ app/src/main/java/com/example/pdv/model/ItemVenda.java
@@ -0,0 +1,44 @@
+package com.example.pdv.model;
+
+public class ItemVenda {
+
+    private int codigoVenda;
+    private int codigoPedido;
+    private double quantidadeProduto;
+    private double valorUnitario;
+
+    public ItemVenda() {
+    }
+
+    public int getCodigoVenda() {
+        return codigoVenda;
+    }
+
+    public void setCodigoVenda(int codigoVenda) {
+        this.codigoVenda = codigoVenda;
+    }
+
+    public int getCodigoPedido() {
+        return codigoPedido;
+    }
+
+    public void setCodigoPedido(int codigoPedido) {
+        this.codigoPedido = codigoPedido;
+    }
+
+    public double getQuantidadeProduto() {
+        return quantidadeProduto;
+    }
+
+    public void setQuantidadeProduto(double quantidadeProduto) {
+        this.quantidadeProduto = quantidadeProduto;
+    }
+
+    public double getValorUnitario() {
+        return valorUnitario;
+    }
+
+    public void setValorUnitario(double valorUnitario) {
+        this.valorUnitario = valorUnitario;
+    }
+}
