e09606283de0b88034405b972c3cea65aa0b5c84
Alan Henrique <alanhipermassa@gmail.com>
2023-11-22 08:26:45 -0300

    Criada a classe Cliente

---
+++ app/src/main/java/com/example/pdv/model/Cliente.java
@@ -0,0 +1,35 @@
+package com.example.pdv.model;
+
+public class Cliente {
+
+    private int codigoCliente;
+    private String nomeCliente;
+    private String documentoCliente;
+
+    public Cliente() {
+    }
+
+    public int getCodigoCliente() {
+        return codigoCliente;
+    }
+
+    public void setCodigoCliente(int codigoCliente) {
+        this.codigoCliente = codigoCliente;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public String getDocumentoCliente() {
+        return documentoCliente;
+    }
+
+    public void setDocumentoCliente(String documentoCliente) {
+        this.documentoCliente = documentoCliente;
+    }
+}
