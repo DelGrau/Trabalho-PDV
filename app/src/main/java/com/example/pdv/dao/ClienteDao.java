808f10d5a829ee96b2386705efba7f00c6235e9f
adrianlucast <adrian.toderke@edu.unipar.br>
2023-11-22 21:40:38 -0300

    Finalizadas funções da classe ClienteDao

--- app/src/main/java/com/example/pdv/dao/ClienteDao.java
+++ app/src/main/java/com/example/pdv/dao/ClienteDao.java
@@ -2,10 +2,13 @@ package com.example.pdv.dao;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
+
+import androidx.appcompat.widget.ViewUtils;
 
 import com.example.pdv.helper.SQLiteDataHelper;
 import com.example.pdv.model.Cliente;
@@ -67,21 +70,86 @@ public class ClienteDao implements GenericDao<Cliente> {
 
     @Override
     public long update(Cliente obj) {
+
+        try {
+
+            ContentValues valores = new ContentValues();
+            valores.put(colunas[1], obj.getNomeCliente());
+            valores.put(colunas[2], obj.getDocumentoCliente());
+
+            String[]identificador = {String.valueOf(obj.getCodigoCliente())};
+            bd.update(tableName, valores, colunas[0]+" = ?", identificador);
+
+        } catch (SQLException ex) {
+            Log.e("ERRO", "ERRO: ClienteDao.update " + ex.getMessage());
+        }
+
         return 0;
     }
 
     @Override
     public long delete(Cliente obj) {
+
+        try {
+            String[]identrificador = {String.valueOf(obj.getCodigoCliente())};
+
+            return bd.delete(tableName, colunas[0]+" = ?", identrificador);
+
+        } catch (SQLException ex) {
+            Log.e("ERRO", "ERRO: ClienteDao.delete " + ex.getMessage());
+        }
+
         return 0;
     }
 
     @Override
     public ArrayList<Cliente> getAll() {
-        return null;
+        ArrayList<Cliente> lista = new ArrayList<Cliente>();
+
+        try {
+            Cursor cursor = bd.query(tableName, colunas,
+                    null, null, null, null,
+                    colunas[0]);
+
+            if (cursor.moveToFirst()) {
+                do {
+                    Cliente cliente = new Cliente();
+                    cliente.setCodigoCliente(cursor.getInt(0));
+                    cliente.setNomeCliente(cursor.getString(1));
+                    cliente.setDocumentoCliente(cursor.getString(2));
+
+                    lista.add(cliente);
+                } while (cursor.moveToNext());
+
+            }
+
+        } catch (SQLException ex) {
+            Log.e("ERRO", "ERRO: ClienteDao.getAll() "+ex.getMessage());
+        }
+        return lista;
     }
 
     @Override
     public Cliente getById(int id) {
+
+        try {
+            String[]identificador = {String.valueOf(id)};
+            Cursor cursor = bd.query(tableName, colunas, colunas[0]+" = ?", identificador,
+                    null, null, null);
+
+            if (cursor.moveToFirst()) {
+                Cliente cliente = new Cliente();
+                cliente.setCodigoCliente(cursor.getInt(0));
+                cliente.setNomeCliente(cursor.getString(1));
+                cliente.setDocumentoCliente(cursor.getString(2));
+
+                return cliente;
+            }
+
+        } catch (SQLException ex) {
+            Log.e("ERRO", "ERRO: ClienteDao.getById() " + ex.getMessage());
+        }
+
         return null;
     }
 }
