87f46571ba9c8f5406452ab87404806dfa017588
Alan Henrique <alanhipermassa@gmail.com>
2023-11-26 23:28:30 -0300

    Alterada versão do Compilador

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.pdv"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.pdv"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
